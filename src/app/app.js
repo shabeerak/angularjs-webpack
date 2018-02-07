61a6554fe2fe814d4348721be8eedddc2e6730ad
skarim <EllieMae1>
2018-02-07 08:48:12 -0800

    adding charts into framework

--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,6 @@
 import angular from 'angular';
+import 'chart.js';
+import 'angular-chart.js';
 
 import '../style/app.css';
 
@@ -12,13 +14,40 @@ let app = () => {
 
 class AppCtrl {
   constructor() {
-    this.url = 'https://github.com/preboot/angular-webpack';
+    this.labels = ["January", "February", "March", "April", "May", "June", "July"];
+    this.series = ['Series A', 'Series B'];
+    this.data = [
+      [65, 59, 80, 81, 56, 55, 40],
+      [28, 48, 40, 19, 86, 27, 90]
+    ];
+    this.onClick = function(points, evt) {
+      console.log(points, evt);
+    };
+    this.datasetOverride = [{yAxisID: 'y-axis-1'}, {yAxisID: 'y-axis-2'}];
+    this.options = {
+      scales: {
+        yAxes: [
+          {
+            id: 'y-axis-1',
+            type: 'linear',
+            display: true,
+            position: 'left'
+          },
+          {
+            id: 'y-axis-2',
+            type: 'linear',
+            display: true,
+            position: 'right'
+          }
+        ]
+      }
+    };
   }
 }
 
 const MODULE_NAME = 'app';
 
-angular.module(MODULE_NAME, [])
+angular.module(MODULE_NAME, ['chart.js'])
   .directive('app', app)
   .controller('AppCtrl', AppCtrl);
 
